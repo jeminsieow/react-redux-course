e6cb47a0dfb7948a05aaa85bbfcb133708aa6c05
Jemin Sieow <sjemin7@gmail.com>
2019-09-30 12:19:49 -0400

    made a todolist, added delete functionality

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Todos from './Todos'
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'learn react'},
+      {id: 2, content: 'work on CSC project'}
+    ]
+  };
 
-    </div>
-  );
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({todos})
+  };
+
+  render() {
+    return (
+      <div className="todo-app container">
+        <h2 className="center">To do list</h2>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    )
+  }
 }
 
 export default App;
